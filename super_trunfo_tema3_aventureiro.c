d5944c6f697ad0855dfea68c550b1561a543d7e8
roither88 <202508428776@alunos.estacio.br>
2025-10-21 18:18:41 +0000

    Melhora a interação do usuário no jogo Super Trunfo, ajustando mensagens de entrada e formatando saídas de comparação entre países.

--- super_trunfo_tema3_aventureiro.c
+++ super_trunfo_tema3_aventureiro.c
@@ -2,16 +2,14 @@
     int main() {
 
     //variáveis para armazenar os dados
-    char pais1[40], pais2[40], codigo1[20], codigo2[20];
+    char pais1[40], pais2[40];
     int populacao1, populacao2, pturisticos1, pturisticos2, escolha;
     float area1, area2, pib1, pib2, densidade1, densidade2;
 
 
     //campos para o usuário digitar as informações do primeiro estado
-    printf("Digite o pais 1 abaixo: \n");
+    printf("Digite o nome do pais 1 abaixo: \n");
     scanf("%s", pais1);
-    printf("Digite o código da carta abaixo: \n");
-    scanf("%s", codigo1);
     printf("Digite a população do pais abaixo: \n");
     scanf("%d", &populacao1);
     printf("Digite a área do pais em Km² abaixo: \n");
@@ -27,10 +25,8 @@
   
 
     //campos para o usuário digitar as informações do segundo estado
-    printf("Digite o pais 2 abaixo: \n");
+    printf("Digite o nome do pais 2 abaixo: \n");
     scanf("%s", pais2);
-    printf("Digite o código da carta abaixo: \n");
-    scanf("%s", codigo2);
     printf("Digite a população desse pais abaixo: \n");
     scanf("%d", &populacao2);
     printf("Digite a área do pais em Km² abaixo: \n");
@@ -58,23 +54,53 @@
     {
     case 1:
         if (populacao1 > populacao2) {
-            printf("Carta %s vence na população com: %d habitantes\n", pais1, populacao1);
+            printf("%s população: %d - %s população: %d = %s venceu\n", pais1, populacao1, pais2, populacao2, pais1);
         } else if (populacao1 < populacao2) {
-            printf("Carta %s vence na população com: %d habitantes\n", pais2, populacao2);
+            printf("%s população: %d - %s população: %d = %s vence\n", pais2, populacao2, pais1, populacao1, pais2);
         } else {
-            printf("####### EMPATE ######");
+            printf("####### EMPATE ######\n");
         }
         break;
     case 2:
         if (area1 > area2) {
-        printf("Carta %s vence na área com: %.2f Km²\n", pais1, area1);
-    } else if (area2 > area1) {
-        printf("Carta %s vence na área com: %.2f Km²\n", pais1, area2);
+           printf("%s área: %.2f - %s área: %.2f = %s venceu\n", pais1, area1, pais2, area2, pais1);
+        } else if (area2 > area1) {
+            printf("%s área: %.2f - %s área: %.2f = %s venceu\n", pais2, area2, pais1, area1, pais2);
+        } else {
+            printf("####### EMPATE ######\n");
+        }
+        break;
+        case 3:
+        if (pib1 > pib2) {
+            printf("%s PIB: %.2f - %s PIB: %.2f = %s venceu\n", pais1, pib1, pais2, pib2, pais1);
+        } else if (pib2 > pib1) {
+            printf("%s PIB: %.2f - %s PIB: %.2f = %s venceu\n", pais2, pib2, pais1, pib1, pais2);
+        } else {
+            printf("####### EMPATE ######\n");
+        }
+        break;
+        case 4:
+        if (pturisticos1 > pturisticos2) {
+            printf("%s pontos turísticos: %d - %s pontos turísticos: %d = %s venceu\n", pais1, pturisticos1, pais2, pturisticos2, pais1);
+        } else if (pturisticos2 > pturisticos1) {
+            printf("%s pontos turísticos: %d - %s pontos turísticos: %d = %s venceu\n", pais2, pturisticos2, pais1, pturisticos1, pais2);
+        } else {
+            printf("####### EMPATE ######\n");
+        }
+        break;
+        case 5:
+        if (densidade1 < densidade2) {
+            printf("%s densidade: %.2f - %s densidade: %.2f = %s venceu\n", pais1, densidade1, pais2, densidade2, pais1);
+        } else if (densidade2 < densidade1) {
+            printf("%s densidade: %.2f - %s densidade: %.2f = %s venceu\n", pais2, densidade2, pais1, densidade1, pais2);
+        } else {
+            printf("####### EMPATE ######\n");
+        }
+        break;
+    default:
+        printf("Opção Inválida\n");
         break;
     }
-    default:
-        printf("Opção Inválidade");
-        break;
 
     return 0;
-    }
+}
